3f7714745c983aea09b369c3e6c147fcfeabfbc0
Davide <davide.orsi1@gmailcom>
2015-12-26 15:42:28 +0100

    fix position in all vicenda

--- app/views/vicendas/all.blade.php
+++ app/views/vicendas/all.blade.php
@@ -12,6 +12,10 @@
 		{{ Form::model($vicenda, array('method' => 'PUT', 'url' => 'vicenda/'.$vicenda->ID ,'class'=>'pure-form pure-form-aligned')) }}
 		
 		<div class='row' style='position:relative; border-left: 5px solid {{$vicenda->color}}; margin-bottom:20px;'>
+	        <div class="form-group col-xs-1 ">
+				{{ Form::button('<span class="glyphicon glyphicon-floppy-disk"></span>', array('type'=>'submit','class' => 'btn btn-default btn-flat')) }}
+			</div>
+			
 	        <div class="form-group col-xs-6 ">
 	        <div class="input-group">
 				<span class="input-group-addon" id="basic-addon-titolo">Titolo</span>
@@ -20,7 +24,7 @@
 			</div>
 			</div>
 
-	        <div class="form-group col-xs-6 ">
+	        <div class="form-group col-xs-5 ">
 	        <div class="input-group">
 				<span class="input-group-addon" id="basic-addon-trama">Trama</span>
 				{{ Form::select('trama', $selectTrama, null, ['class'=>'form-control','describedby'=>"basic-addon-trama"])}}
@@ -30,26 +34,28 @@
 	        <div class="form-group col-xs-12" style='position:relative; top:-25px;'>
 				{{ Form::textarea('body', null, ['class'=>' editor', 'style'=>'height:auto']) }}	
 			</div>
-			{{ Form::button('<span class="glyphicon glyphicon-floppy-disk"></span>', array('type'=>'submit','class' => 'btn btn-default btn-lg btn-flat', 'style'=>'position:absolute; top:0px; left:-60px;')) }}
 			
 		{{ Form::close() }}
 			<!-- ELEMENTI #########################################-->
 			@foreach($vicenda->schedule as $elemento)
 				<div class='col-xs-12' style='position:relative; top:-50px; margin-bottom:-50px;'>
 					{{ Form::model($elemento, array('method' => 'PUT', 'url' => 'elemento/'.$elemento->ID ,'class'=>'pure-form pure-form-aligned','id'=>'form_edit' )) }}
-					<div class="form-group col-xs-offset-1 col-xs-11 " style='border-left: solid 5px #ccc; padding-bottom:5px;'>
+					<div class="form-group col-xs-offset-1 col-xs-1 " style='border-left: solid 5px #ccc; padding-bottom:5px;'>
+					{{ Form::button('<span class="glyphicon glyphicon-floppy-disk"></span>', array('type'=>'submit','class' => 'btn btn-default btn-flat')) }}
+					</div>
+					<div class="form-group col-xs-10 " style=' padding-bottom:5px;'>
 						<div class="input-group">
 							<span class="input-group-addon" id="basic-addon-titolo">Titolo</span>
 							{{ Form::text('text',Input::old('text'), ['class'=>'form-control','describedby'=>"basic-addon-titolo"]) }}
 						</div>
 					</div>
-					<div class="form-group col-xs-offset-1 col-xs-5 " style='border-left: solid 5px #ccc; top:-15px;'>
+					<div class="form-group col-xs-offset-1 col-xs-5 " style='border-left: solid 5px #ccc; top:-16px;'>
 						<div class="input-group">
 							<span class="input-group-addon" id="basic-addon-inizio">Inizio</span>
 							{{ Form::text('start',Input::old('start'), ['class'=>'form-control','describedby'=>"basic-addon-inizio"]) }}
 						</div>
 					</div>
-					<div class="form-group col-xs-offset-1 col-xs-5 " style=' top:-15px;'>
+					<div class="form-group col-xs-offset-1 col-xs-5 " style=' top:-16px;'>
 						<div class="input-group">
 							<span class="input-group-addon" id="basic-addon-fine">Fine</span>
 							{{ Form::text('end',Input::old('end'), ['class'=>'form-control','describedby'=>"basic-addon-fine"]) }}
@@ -60,7 +66,6 @@
 					{{ Form::hidden('vicenda', $vicenda->ID, ['id'=>'vicenda'])}}
 					{{ Form::textarea('data', Input::old('data'), ['id'=>'data','class'=>' form-control', 'style'=>'width:100%; margin-bottom:0px']) }}
 					</div>
-					{{ Form::button('<span class="glyphicon glyphicon-floppy-disk"></span>', array('type'=>'submit','class' => 'btn btn-default btn-lg btn-flat', 'style'=>'position:absolute; top:0px; left:45px;')) }}
 				
 					{{ Form::close() }}
 				</div>
