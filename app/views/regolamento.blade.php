e551c38db986f91463f3800bd33a132476817912
Davide <davide.orsi1@gmailcom>
2016-08-22 10:30:20 +0200

    tomo regole 4.4

--- app/views/regolamento.blade.php
+++ app/views/regolamento.blade.php
@@ -16,7 +16,7 @@
 
 <div class='col-sm-6' style='text-align: center;'>
 
-	<a href={{URL::to('files/tomo_delle_regole_4_3.pdf')}}>
+	<a href={{URL::to('files/tomo_delle_regole_4_4.pdf')}}>
 	{{ HTML::image('img/regole.jpg','Regolamento',array('style'=>'width:100%; max-width: 300px;'))}}
 	</a>
 
