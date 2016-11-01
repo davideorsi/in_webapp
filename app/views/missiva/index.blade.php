c9cc6526506e019fb79a48776e61beb0cfc0e5e6
Davide <davide.orsi1@gmailcom>
2016-11-01 12:20:33 +0100

    Ridisegnata la grafica delle missive

--- app/views/missiva/index.blade.php
+++ app/views/missiva/index.blade.php
@@ -13,7 +13,7 @@
 		<!-- SEARCH LATERAL MENU -->
 		<div class='col-sm-4'>
 			
-			<h2 >Missive</h2>
+			<h2 class='hidden-xs'>Missive</h2>
 			
 			@if (Auth::user()->usergroup == 7)
 			{{Form::open(array('onsubmit'=>'get_list_missive(1,1); return false;')) }}
@@ -28,58 +28,60 @@
 				</div>
 			</div>
 			
-			<div class="form-group" > 
-				<div class="input-group margin-bottom">
-				<span class="input-group-addon" id="basic-addon-PNG">PNG</span>	
-				{{ Form::text('PNG', Input::old('PNG'), ['class'=>'form-control','describedby'=>"basic-addon-PNG",'placeholder'=>'Nome del PNG']) }}
+			<div id="other_inputs" class="hidden-xs">
+				<div class="form-group" > 
+					<div class="input-group margin-bottom">
+					<span class="input-group-addon" id="basic-addon-PNG">PNG</span>	
+					{{ Form::text('PNG', Input::old('PNG'), ['class'=>'form-control','describedby'=>"basic-addon-PNG",'placeholder'=>'Nome del PNG']) }}
+					</div>
+				</div>
+				
+				
+				<div class="form-group" > 
+					<div class="input-group margin-bottom">
+					<span class="input-group-addon" id="basic-addon-testo">Testo</span>	
+					{{ Form::text('Testo', Input::old('testo'), ['class'=>'form-control','describedby'=>"basic-addon-testo",'placeholder'=>'Parole chiave, es: "spia"']) }}
+					</div>
+				</div>
+	
+	
+	
+				<div class="form-group" > 
+					<div class="input-group margin-bottom">
+					<span class="input-group-addon" id="basic-addon-data">Data</span>	
+					{{ Form::text('Data', Input::old('Data'), ['class'=>'form-control','describedby'=>"basic-addon-data"]) }}
+					</div>
+				</div>
+	
+				@if (Auth::user()->usergroup == 7)
+				<div   class="form-group"  title='Mostra solo le missive intercettate'>
+					<div class="input-group margin-bottom">
+					<span class="input-group-addon" id="basic-addon-intercettato"><span class="glyphicon glyphicon-flash" style="color:#FF6600;"></span></span>	
+					{{ Form::checkbox('Intercettato',1, Input::old('intercettato'), ['class'=>'checkbox','describedby'=>"basic-addon-intercettato"]) }}
+					</div>
+				</div>
+				<div  class="form-group"   title='Mostra solo le missive a cui dobbiamo ancora rispondere'>
+					<div class="input-group margin-bottom">
+					<span class="input-group-addon" id="basic-addon-nonrisp">
+						<span class="glyphicon glyphicon-check" style="color: #f00"></span>
+					</span>	
+					{{ Form::checkbox('Solononrisp',1, Input::old('rispondere'), ['class'=>'checkbox','describedby'=>'basic-addon-nonrisp']) }}
+					</div>
+				</div>
+				@endif
+	
+				<div  class="form-group" title='Mostra solo le missive per PNG'>
+					<div class="input-group ">
+					<span class="input-group-addon" id="basic-addon-solopng">
+						Solo PNG
+					</span>	
+					{{ Form::checkbox('ConPNG',1, Input::old('ConPNG'), ['class'=>'checkbox','describedby'=>'basic-addon-solopng']) }}
+					</div>
 				</div>
 			</div>
-			
-			
-			<div class="form-group" > 
-				<div class="input-group margin-bottom">
-				<span class="input-group-addon" id="basic-addon-testo">Testo</span>	
-				{{ Form::text('Testo', Input::old('testo'), ['class'=>'form-control','describedby'=>"basic-addon-testo",'placeholder'=>'Parole chiave, es: "spia"']) }}
-				</div>
-			</div>
 
-
-
-			<div class="form-group" > 
-				<div class="input-group margin-bottom">
-				<span class="input-group-addon" id="basic-addon-data">Data</span>	
-				{{ Form::text('Data', Input::old('Data'), ['class'=>'form-control','describedby'=>"basic-addon-data"]) }}
-				</div>
-			</div>
-
-			@if (Auth::user()->usergroup == 7)
-			<div   class="form-group"  title='Mostra solo le missive intercettate'>
-				<div class="input-group margin-bottom">
-				<span class="input-group-addon" id="basic-addon-intercettato"><span class="glyphicon glyphicon-flash" style="color:#FF6600;"></span></span>	
-				{{ Form::checkbox('Intercettato',1, Input::old('intercettato'), ['class'=>'checkbox','describedby'=>"basic-addon-intercettato"]) }}
-				</div>
-			</div>
-			<div  class="form-group"   title='Mostra solo le missive a cui dobbiamo ancora rispondere'>
-				<div class="input-group margin-bottom">
-				<span class="input-group-addon" id="basic-addon-nonrisp">
-					<span class="glyphicon glyphicon-check" style="color: #f00"></span>
-				</span>	
-				{{ Form::checkbox('Solononrisp',1, Input::old('rispondere'), ['class'=>'checkbox','describedby'=>'basic-addon-nonrisp']) }}
-				</div>
-			</div>
-			@endif
-
-			<div  class="form-group" title='Mostra solo le missive per PNG'>
-				<div class="input-group ">
-				<span class="input-group-addon" id="basic-addon-solopng">
-					Solo PNG
-				</span>	
-				{{ Form::checkbox('ConPNG',1, Input::old('ConPNG'), ['class'=>'checkbox','describedby'=>'basic-addon-solopng']) }}
-				</div>
-			</div>
-
-
-			<div>  
+			<div class='btn-group'> 
+				<a class='btn btn-primary visible-xs-block' title='Mostra ricerca avanzata'><span class='glyphicon glyphicon-plus' href='#' onclick='mostra_controlli();'></span></a> 
 				{{ Form::submit('Cerca', array('class' => 'btn btn-success ')) }}
 			</div>
 
@@ -90,21 +92,26 @@
 
 	
 	
-	{{--##### PAGINATOR ###########################################--}}
-	<div class='col-sm-8' id='pagine'></div>
+
+
 
 	
 	{{--##### LIST OF ELEMENTS ###########################################--}}
-	<div class='col-sm-8'>
-		<div >
-			<ul class='media-list' id='results'></ul> 
-		
+	<div class='col-sm-8' style='position:relative;'>
+		<div id='parent-list' style='position: absolute; top: 40px; left:0px; width:100%'>
+			<div id='pagine' class='col-sm-8'></div>
+			<div class='col-sm-12'>
+				<ul class='media-list' id='results'></ul> 
+			</div>
 		</div>
-		<div id='lateral_panel' class='hidden'>
-			<div class='media-body'>
+		<div id='panel_missiva' style='position: absolute; top: 40px; left:10px; padding: 0px; background-color: #fff;'>
+			<div class='col-sm-12' class='media-body'>
+				
+				
+				<div id='missiva_icon_bar'></div>
 				<header>
-					<h5 id='mittente_sm'></h5>
-					<h5 id='destinatario_sm'></h5>
+					<h4 id='mittente_sm'></h4>
+					<h4 id='destinatario_sm'></h4>
 				</header>
 				
 				<p id='testo_sm' class='justified '></p>
@@ -116,6 +123,7 @@
 @stop
 
 @section('JS')
+	{{ HTML::script('js/jquery-ui.min.js');}}
 @stop
 
 @section('Scripts')
@@ -126,9 +134,16 @@
 			var show_delete=false;
 		@endif
 		get_list_missive(1,show_delete);
-		
-	
+		$('#panel_missiva').toggle(0);
+		$('.bodyfooter').hide()
 	});
+	
+	function hide_missiva() {
+			$('#panel_missiva').toggle(0);
+		}
 
+	function mostra_controlli() {
+			$('#other_inputs').toggle().removeClass('hidden-xs');
+		}
 
 @stop
