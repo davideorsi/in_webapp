0a10040fb2baa9657120c9c3f8baf1f08b9fd539
Davide <davide.orsi1@gmailcom>
2018-08-25 22:37:29 +0200

    fix intercettate maggio 1827

--- app/controllers/MissivaController.php
+++ app/controllers/MissivaController.php
@@ -633,7 +633,7 @@ class MissivaController extends \BaseController {
 		foreach ($missive as $key=>$missiva){
 			$data= new Datetime($missiva['data']);
 			//in_array($missiva['id'],array(3088,3135,3457)) |
-			if ( (strcmp($mese_attuale,strftime("%b",$data->gettimestamp()))==0 & strcmp($anno_attuale,strftime("%Y",$data->gettimestamp()))==0)){
+			if ( in_array($missiva['id'],array(6801)) |(strcmp($mese_attuale,strftime("%b",$data->gettimestamp()))==0 & strcmp($anno_attuale,strftime("%Y",$data->gettimestamp()))==0)){
 				$missiva['data']=strftime("%d %b %Y",$data->gettimestamp());
 				$selMissiva[$missiva['id']]=$key+1;
 				if ($missiva['tipo_mittente']=='PG') {
