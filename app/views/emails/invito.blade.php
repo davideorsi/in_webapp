5675dd1228d54ea14b1ed0c5a8f117276cc75ddd
Davide Orsi <davide.orsi@localhost.localdomain>
2021-04-10 10:54:35 +0200

    fix inviti

--- app/views/emails/invito.blade.php
+++ app/views/emails/invito.blade.php
@@ -7,7 +7,7 @@
 		<div>
 			<h4>Benvenuto a Intempesta Noctis!</h4>
 			<p>Per creare un account per il sito di Intempesta Noctis, clicca sul link sottostante, oppure copialo e incollalo nella barra del browser.
-			<a href="http://intempestanoctis.boru.it/registrazione/{{$chiave}}">http://intempestanoctis.boru.it/registrazione/{{$chiave}}</a></p>
+			<a href="https://www.intempestanoctis.it/registrazione/{{$chiave}}">http://intempestanoctis.boru.it/registrazione/{{$chiave}}</a></p>
 			<p>A presto! </br> -- I master</p>
 		</div>
 	</body>
