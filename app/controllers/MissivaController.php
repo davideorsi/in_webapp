952964cb11c3c0f0da6d182104b0f943407bf2a4
Davide Orsi <davide.orsi1@gmail.com>
2021-07-15 21:48:22 +0200

    Fix intercettate post pandemia

--- app/controllers/MissivaController.php
+++ app/controllers/MissivaController.php
@@ -672,7 +672,7 @@ class MissivaController extends \BaseController {
 		foreach ($missive as $key=>$missiva){
 			$data= new Datetime($missiva['data']);
 			//in_array($missiva['id'],array(3088,3135,3457)) |
-			if ((strcmp($mese_attuale,strftime("%b",$data->gettimestamp()))==0 & strcmp($anno_attuale,strftime("%Y",$data->gettimestamp()))==0)){
+			if (in_array($missiva['id'],array(8558,8567,8974,8990,9036,9094,9128)) |(strcmp($mese_attuale,strftime("%b",$data->gettimestamp()))==0 & strcmp($anno_attuale,strftime("%Y",$data->gettimestamp()))==0)){
 				$missiva['data']=strftime("%d %b %Y",$data->gettimestamp());
 				$selMissiva[$missiva['id']]=$key+1;
 				if ($missiva['tipo_mittente']=='PG') {
