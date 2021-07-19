141b143afa9f749128e2eb05e65ad0e4abb4da81
Davide Orsi <davide.orsi1@gmail.com>
2021-07-19 21:56:35 +0200

    Mappa

--- app/views/mappa.blade.php
+++ app/views/mappa.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class='row'>
 <div class='col-sm-10 col-sm-offset-1'>	
-		<p>   </p>
+		<p>Questa mappa riproduce, a grandi linee, la conformazione degli stati del nord della penisola all'epoca del mondo di gioco. La posizione di specifiche città, e la loro attribuzione ad uno specifico stato, potrebbe non riflettere accuratamente il mondo di Intempesta Noctis.</p>
 		<img style="width: 100%; max-width: 100%;" src="https://upload.wikimedia.org/wikipedia/commons/c/c3/North_Italy%2C_1828_%28Hall%29.jpg"></img>
 </div>
 </div>
